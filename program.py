6256bb5e3d7f7d5b8cb0e0085831cfc3060f9957
siddiqui <aurangzaibsiddiqui@gmail.com>
2017-05-30 11:43:48 +0200

    updated the writeup -- reference the images in each step of the pipeline.

--- program.py
+++ program.py
@@ -1,121 +1,245 @@
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 import numpy as np
 import cv2 as cv
+import time
+
+SHOW_DEBUG_IMAGES = False
 
 
-def perform_lane_detection():
-    video_cap = cv.VideoCapture(
-        '/Users/siddiqui/Documents/Projects/self-drive/CarND-LaneLines-P1/test_videos/solidYellowLeft.mp4')
+def save_image(image, name="unknown", path="result_images/"):
+    stamp = str(int(time.time()))
+    cv.imwrite(path + name + "-" + stamp + ".jpg", image)
+
+
+def perform_lane_detection(path="test_videos/challenge.mp4"):
+    video_cap = cv.VideoCapture(path)
     while video_cap.isOpened():
         success, frame = video_cap.read()
         if not success:
             print("cannot read frame")
             return -1
-        detect_lane(frame)
+        lanes_detection(frame)
 
 
-def detect_lane(image):
-    # image = cv.imread("test_images/solidYellowCurve2.jpg")
-    image = cv.resize(image, None, fx=0.5, fy=0.6, interpolation=cv.INTER_LINEAR)
-    # gray scale and noise reduction
+def get_height_width(image):
+    return image.shape[1], image.shape[0], image.shape[0] * .62
+
+
+def get_masked_image(image):
+    # HSV image
     image_hsv = cv.cvtColor(image, cv.COLOR_BGR2HSV)
-    image_gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-    # define range of blue color in HSV
-    cv.imshow("hsv", image_hsv)
 
+    # gray-scale image
+    image_gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    # get region of image within yellow range
     mask_yellow = cv.inRange(image_hsv,
                              np.array([20, 100, 100]),  # yellow lower
                              np.array([100, 255, 255]))  # yellow upper
 
+    # get region of image within white range
     mask_white = cv.inRange(image_hsv,
                             np.array([0, 0, 240]),  # white lower
                             np.array([255, 255, 255]))  # white lower
 
+    # merge results of yellow and white - bitwise OR
     mask_hsv = cv.bitwise_or(mask_yellow, mask_white)
-    cv.imshow("1- white and yellow masking", image_hsv)
-
-    # only retain the information of the lines in the image
+    # only retain the information of the lanes in the image - bitwise AND
     masked_image = cv.bitwise_and(image_gray, mask_hsv)
 
-    # remove noise using gaussian filter with 9x9 kernel size
-    gray = cv.GaussianBlur(masked_image,  # source
-                           (9, 9),  # kernel size
-                           0)  # border type
-    cv.imshow("2- after noise removal", masked_image)
+    if SHOW_DEBUG_IMAGES:
+        save_image(image_hsv, "01-hsv")
+        save_image(mask_yellow, "02-yellow")
+        save_image(mask_white, "03-white")
+        save_image(mask_hsv, "04-mask hsv")
+        save_image(mask_hsv, "05-mask hsv")
+        cv.imshow("06-masked image", masked_image)
+    return masked_image
 
-    # find the edges
-    edges = cv.Canny(gray,
-                     50,  # low threshold
-                     150)  # high threshold
-    cv.imshow("3- canny edge detector", edges)
-    width = image.shape[1]
-    height = image.shape[0]
 
+def remove_image_noise(image, kernel=(3, 3)):
+    filtered_image = cv.GaussianBlur(image,  # source
+                                     kernel,  # kernel size
+                                     0)  # border type
+    if SHOW_DEBUG_IMAGES:
+        save_image(filtered_image, "07-filtered")
+        cv.imshow("2- after noise removal", filtered_image)
+    return filtered_image
+
+
+def get_edges(image, low, high):
+    edges = cv.Canny(image,
+                     low,  # low threshold
+                     high)  # high threshold
+    if SHOW_DEBUG_IMAGES:
+        save_image(edges, "08-canny")
+        cv.imshow("3- canny edge detector", edges)
+    return edges
+
+
+def get_masked_edges(width, height_bottom, height_top, edges, ignore_mask_color=255):
+    # blank matrix
     mask = np.zeros_like(edges)
-    ignore_mask_color = 255
-    vertices = np.array([[(width * 0.05, height),  # left bottom
-                          (width / 4., height * .589),  # left top
-                          (3 * width / 4., height * .589),  # right top
-                          (width * 0.95, height)]],  # right bottom
+    # ROI vertices
+    vertices = np.array([[(width * 0.05, height_bottom),  # left bottom
+                          (width / 4., height_top),  # left top
+                          (3 * width / 4., height_top),  # right top
+                          (width * 0.95, height_bottom)]],  # right bottom
                         dtype=np.int32)
-
+    # create ROI
     cv.fillPoly(mask,  # image
                 vertices,  # coordinates
                 ignore_mask_color)
-    cv.imshow("ROI", mask)
-    # remove the parts of image which are not within vertices
+    # remove the parts of image which are not within ROI - bitwise AND
     masked_edges = cv.bitwise_and(edges, mask)
+    if SHOW_DEBUG_IMAGES:
+        save_image(mask, "09-ROI")
+        save_image(masked_edges, "10-masked_edges")
+        cv.imshow("ROI", mask)
+        cv.imshow("masked edges", masked_edges)
 
-    # find lines using hough
-    detected_lines = cv.HoughLinesP(masked_edges,  # source
-                                    2,  # rho --> 1 pixel
-                                    1 * np.pi / 180,  # theta in radian (1 degree)
-                                    15,  # min voting
-                                    10,  # min line length in pixels
-                                    1  # max line gap in pixels
-                                    )
+    return masked_edges
 
+
+def get_hough_lines(image, rho=2, theta=1, voting=15,
+                    min_line_length=10, max_line_gap=1):
+    return cv.HoughLinesP(image,  # source
+                          rho,
+                          theta * np.pi / 180,  # degree to radian
+                          voting,  # minimum voting in hough accumulator
+                          min_line_length,  # min line length in pixels
+                          max_line_gap  # gap b/w lines in pixels
+                          )
+
+
+def get_weighted_lanes(detected_lines):
+    # hough line transform provides several detected lines
+    # we want to reduce all the to just 2 lines
+    # lines will be weighted w.r.t the length of the lines
+    # resultant lines are called lanes (left and right)
+    right_slope_intercept = []
+    right_length = []
+    left_slope_intercept = []
+    left_length = []
+    for line in detected_lines:
+        # get start and end coordinates of each line
+        for x1, y1, x2, y2 in line:
+            # ignore same coordinate to avoid infinite slope
+            if x1 == x2:
+                continue
+            # slope of line
+            line_slope = float(y2 - y1) / float(x2 - x1)
+            # angle of deviation
+            line_angle = np.arctan2(y2 - y1, x2 - x1) * 180.0 / np.pi
+            # y intercept
+            line_intercept = y1 - line_slope * x1
+            # line length, used for weight
+            line_length = np.sqrt((y2 - y1) ** 2 + (x2 - x1) ** 2)
+            # in openCV, image is a matrix
+            # bottom y values are higher than top y values
+            # it means slope is negative for left line
+
+            # left slope = delta_y --> -ve, delta_x --> +ve
+            # right slope = delta_y --> -ve, delta_x --> -ve
+            if line_slope < 0:
+                left_slope_intercept.append((line_slope, line_intercept))
+                left_length.append(line_length)
+            else:
+                right_slope_intercept.append((line_slope, line_intercept))
+                right_length.append(line_length)
+
+    # weighted mean
+    # https://en.wikipedia.org/wiki/Weighted_arithmetic_mean
+    # simple arithmetic mean doesn't take into account the ...
+    # ...significance of individual lines which causes fluctuation & noise
+    # length of each line can be used as a weight
+    # weight average --> (sum of product) / (sum of weight)
+    # sop --> sum of product
+    sop_left = np.dot(left_length, left_slope_intercept)
+    sop_right = np.dot(right_length, right_slope_intercept)
+    left_weight = np.sum(left_length)
+    right_weight = np.sum(right_length)
+    # handling cases when no line is detected by hough transform
+    left = (sop_left / left_weight) if len(left_length) > 0 else None
+    right = (sop_right / right_weight) if len(right_length) > 0 else None
+
+    # resultant is 2 lanes out of several hough lines
+    return left, right
+
+
+def get_coordinates(height_bottom, height_top, left, right):
+    # start and end x coordinates for left lane
+    # x --> (y - intercept) / slope
+    slope, intercept = left
+    left_x1 = int((height_bottom - intercept) / slope)
+    left_x2 = int((height_top - intercept) / slope)
+    # start and end x coordinates for right lane
+    slope, intercept = right
+    right_x1 = int((height_bottom - intercept) / slope)
+    right_x2 = int((height_top - intercept) / slope)
+    # y coordinates
+    y1 = int(height_bottom)
+    y2 = int(height_top)
+
+    return ((left_x1, y1), (left_x2, y2)), ((right_x1, y1), (right_x2, y2))
+
+
+def draw_lanes(edges, left_coordinates, right_coordinates,
+               color=(0, 0, 255), thickness=5):
     # creating a blank to draw lines on
-    line_image = np.zeros((masked_edges.shape[0],
-                           masked_edges.shape[1],
+    lane_image = np.zeros((edges.shape[0],  # --> height
+                           edges.shape[1],  # --> width
                            3),
                           dtype=np.uint8)
+    for coordinates in (left_coordinates, right_coordinates):
+        start_coordinate, end_coordinate = coordinates
+        cv.line(lane_image,  # source
+                start_coordinate,  # (x1, y1)
+                end_coordinate,  # (x2, y2)
+                color,  # BGR color --> red
+                thickness  # line thickness
+                )
+    save_image(lane_image, "11-lanes")
+    cv.imshow("smooth lines", lane_image)
+    return lane_image
 
-    left_line = []
-    # several lines are detected
-    # each line has start and end points
-    print("line length before:", detected_lines.size)
-    for line in detected_lines:
-        for x1, y1, x2, y2 in line:
-            # if x2 == x1:
-            #     continue
-            slope = (y2 - y1) / (x2 - x1)
-            angle = np.arctan2(y2 - y1, x2 - x1) * 180.0 / np.pi
-            length = np.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 
-    print("line length after:", len(left_line))
-    for line in detected_lines:
-        for x1, y1, x2, y2 in line:
-            if angle != 0 and length >= 1:
-                cv.line(line_image,  # source
-                        (x1, y1),  # start point
-                        (x2, y2),  # end point
-                        (0, 0, 255),  # BGR color --> red
-                        5  # line thickness
-                        )
+def get_resultant_image(source1, source2, alpha=0.8, beta=1, gamma=0):
+    resultant = cv.addWeighted(source1,
+                               alpha,  # weight of the first array elements
+                               source2,  # source 2
+                               beta,  # weight of the second array elements
+                               gamma)  # scalar added to each sum
+    save_image(resultant, "12-resultant")
+    return resultant
 
-    cv.imshow("hough line", line_image)
-    # gray images can't draw colored features on it
-    # this is a way to do it, using dstack and weighted sum
-    # color_edges = np.dstack((edges, edges, edges))
-    line_edges = cv.addWeighted(line_image,  # source 1
-                                0.8,  # alpha --> weight of the first array elements
-                                image,  # source 2
-                                1,  # beta --> weight of the second array elements
-                                0)  # gamma --> scalar added to each sum
 
-    cv.imshow("result", line_edges)
+def lanes_detection(image):
+    # STEP - 0: resize frame to better visualize
+    image = cv.resize(image, None, fx=0.5, fy=0.6, interpolation=cv.INTER_LINEAR)
+    save_image(image, "original")
+    # STEP - 1: get the width and height of image
+    width, height_bottom, height_top = get_height_width(image)
+    # STEP - 2: get the white and yellow lanes from the image
+    masked_image = get_masked_image(image)
+    # STEP - 3: remove noise using gaussian filter with 9x9 kernel size
+    filtered_image = remove_image_noise(masked_image, kernel=(9, 9))
+    # STEP - 4: get canny edge
+    edges = get_edges(filtered_image, low=50, high=150)
+    # STEP - 5: remove edges outside of ROI
+    masked_edges = get_masked_edges(width, height_bottom, height_top, edges)
+    # STEP - 6: find lines using hough
+    hough_lines = get_hough_lines(masked_edges)
+    # STEP - 7: get left and right lanes from several hough lines
+    left_lane, right_lane = get_weighted_lanes(hough_lines)
+    # STEP - 8: get the coordinates of the left and right lanes
+    left_coordinates, right_coordinates = get_coordinates(height_bottom,
+                                                          height_top,
+                                                          left_lane,
+                                                          right_lane)
+    # STEP - 9: draw lanes on a blank image
+    lane_image = draw_lanes(masked_edges, left_coordinates, right_coordinates)
+    # STEP - 10: draw lanes on original image
+    weighted_image = get_resultant_image(lane_image, image)
+    cv.imshow("result", weighted_image)
     cv.waitKey(1)
 
 
--- writeup_template.md
+++ writeup_template.md
@@ -15,7 +15,35 @@ The goals / steps of this project are the following:
 
 [//]: # (Image References)
 
-[image1]: ./examples/grayscale.jpg "Grayscale"
+[image0-1]: ./results/00-original-1496136918.jpg "Scenario"
+[image0-2]: ./results/00-original-1496136931.jpg "Scenario"
+[image0-3]: ./results/00-original-1496136945.jpg "Scenario"
+[image2]: ./results/01-hsv-1496135300.jpg "HSV"
+[image3]: ./results/01-hsv-1496135331.jpg "HSV"
+[image3]: ./results/01-hsv-1496135356.jpg "HSV"
+[image4]: ./results/02-yellow-1496135300.jpg "Yellow Mask"
+[image5]: ./results/02-yellow-1496135355.jpg "Yellow Mask"
+
+[image6]: ./results/03-white-1496135366.jpg "White Mask"
+[image6]: ./results/03-white-1496135329.jpg "White Mask"
+
+[image7]: ./results/04-mask-hsv-1496135309.jpg "Mask HDV"
+[image8]: ./results/04-mask-hsv-1496135361.jpg "Mask HDV"
+
+[image9]: ./results/07-filtered-1496135354.jpg "Filtered"
+[image10]: ./results/07-filtered-1496135303.jpg "Filtered"
+
+[image11]: ./results/08-canny-1496135302.jpg "Edges"
+[image12]: ./results/08-canny-1496135366.jpg "Edges"
+
+[image13]: ./results/09-ROI-1496135301.jpg "ROI"
+
+[image16]: ./results/11-lanes-1496135307.jpg "Lanes"
+[image17]: ./results/11-lanes-1496135366.jpg "Lanes"
+
+[image18]: ./results/12-resultant-1496135312.jpg "Result"
+[image19]: ./results/12-resultant-1496135332.jpg "Result"
+[image20]: ./results/12-resultant-1496135364.jpg "Result"
 
 ---
 
@@ -29,8 +57,27 @@ In order to draw a single line on the left and right lanes, I modified the draw_
 
 If you'd like to include images to show how the pipeline works, here is how to include an image: 
 
-![alt text][image1]
+![alt text][image0-1]
+![alt text][image0-2]
+![alt text][image0-3]
+![alt text][image2]
+![alt text][image3]
+![alt text][image4]
+![alt text][image5]
+![alt text][image6]
+![alt text][image7]
+![alt text][image8]
+![alt text][image9]
+![alt text][image10]
+![alt text][image11]
+![alt text][image12]
+![alt text][image13]
 
+![alt text][image16]
+![alt text][image17]
+![alt text][image18]
+![alt text][image19]
+![alt text][image20]
 
 ### 2. Identify potential shortcomings with your current pipeline
 
